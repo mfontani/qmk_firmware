3026c9b064e908a1631dca72f4a609c115643948
Marco Fontani <MFONTANI@cpan.org>
2017-08-24 19:39:06 +0200

    [ergodox mfontani] place alt+cmd on bottom right LHS

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl |  `   |  '"  | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |LCtrl |  `   |  '"  | Alt  | Cmd  |                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Left |Right |       | Alt  |Ctrl/Esc|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LCTRL,       KC_GRV,       KC_QUOT,      KC_LEFT,KC_RGHT,
+        KC_LCTRL,       KC_GRV,       KC_QUOT,      KC_LALT,KC_LGUI,
                                                       KC_LEFT,KC_RIGHT,
                                                               KC_HOME,
                                                KC_SPC,KC_LGUI,KC_END,
