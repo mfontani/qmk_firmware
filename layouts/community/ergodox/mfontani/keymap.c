1f59ab352a0736286dfa6e1006468e35be5ec6a6
Marco Fontani <MFONTANI@cpan.org>
2017-08-25 14:03:43 +0200

    [ergodox mfontani] move del to RHS board top left cluster

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LCtrl |Ctrl/`|  '"  | Alt  |Cmd/Sp|                                       |  Up  | Down |   [  |   ]  | AltGr|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Left |Right |       | AltGr|Ctrl/Esc|
+ *                                        | Left |Right |       | Del  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space| Del  |------|       |------|  Del   |Enter |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_ENT),
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_RALT,
-             KC_RALT,        CTL_T(KC_ESC),
+             KC_DELT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_DELT, KC_ENT
     ),
