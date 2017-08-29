7cf0815a97e6a97b6a75258a8f2ac31e9600d5f3
Marco Fontani <MFONTANI@cpan.org>
2017-08-29 16:05:02 +0200

    [ergodox mfontani] better alignment of values in layout

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -63,23 +63,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LT(SYMB,KC_LBRC),
-        KC_GRV,         LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_LCTRL,       CTL_T(KC_GRV),KC_QUOT,      KC_LALT, KC_LGUI,
+        KC_ESC,   KC_1,          KC_2,    KC_3,    KC_4,    KC_5, KC_6,
+        KC_TAB,   KC_Q,          KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_LBRC),
+        KC_GRV,   LT(MDIA,KC_A), KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT,  KC_Z,          KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,
+        KC_LCTRL, CTL_T(KC_GRV), KC_QUOT, KC_LALT, KC_LGUI,
                                                       KC_LEFT,KC_RIGHT,
                                                               KC_HOME,
                                                KC_SPC,KC_LGUI,KC_END,
         // right hand
-             KC_7,        KC_8,   KC_9,  KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
-             KC_BSLS,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_QUOT,
-                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_ENT),
-             KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_RALT,
-             KC_DELT,        KC_RALT,
-             KC_PGUP,
-             KC_PGDN,LT(CURS,KC_SPC),LT(CURS,KC_ENT)
+        KC_7,    KC_8, KC_9,  KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
+        KC_BSLS, KC_Y, KC_U,  KC_I,    KC_O,    KC_P,             KC_QUOT,
+                 KC_H, KC_J,  KC_K,    KC_L,    LT(MDIA,KC_SCLN), GUI_T(KC_ENT),
+        KC_RBRC, KC_N, KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),   KC_RSFT,
+                       KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,          KC_RALT,
+        KC_DELT, KC_RALT,
+        KC_PGUP,
+        KC_PGDN, LT(CURS,KC_SPC), LT(CURS,KC_ENT)
     ),
 /* Keymap 1: Media and mouse keys
  *
@@ -104,23 +104,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_WH_U, KC_WH_D,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-    // right hand
-       KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  VRSN,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+                                                KC_WH_U, KC_WH_D,
+                                                         KC_TRNS,
+                                       KC_TRNS, KC_TRNS, KC_TRNS,
+        // right hand
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  VRSN,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
                  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_MUTE, KC_MPRV, KC_MNXT,  KC_UP,   KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_MUTE, KC_MPRV, KC_MNXT,  KC_UP,   KC_TRNS,
                           KC_VOLU, KC_VOLD, KC_RIGHT, KC_DOWN, KC_RIGHT,
-       KC_TRNS, KC_WBAK,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_WBAK,
+        KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Symbol Layer
  *
