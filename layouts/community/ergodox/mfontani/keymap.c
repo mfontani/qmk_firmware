954c82cf1f8e926c542399f479cd52b8333af4d0
Marco Fontani <MFONTANI@cpan.org>
2017-08-30 11:17:44 +0200

    [ergodox mfontani] replace SYMB tap with /

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   =  |Backspac|
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  \   |   Y  |   U  |   I  |   O  |   P  |   '"   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  /   |           |  \   |   Y  |   U  |   I  |   O  |   P  |   '"   |
  * |--------+------+------+------+------+------| /SYMB|           |      |------+------+------+------+------+--------|
  * |   `~   |A / MD|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/Cmd |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,   KC_1,          KC_2,    KC_3,    KC_4,    KC_5, KC_6,
-        KC_TAB,   KC_Q,          KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_LBRC),
+        KC_TAB,   KC_Q,          KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
         KC_GRV,   LT(MDIA,KC_A), KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,  KC_Z,          KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,
         KC_LCTRL, CTL_T(KC_GRV), KC_SLSH, KC_LALT, KC_LGUI,
