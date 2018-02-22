4bd5f88d1c4e05df1544378dc21a2d85e38fa71a
Marco Fontani <MFONTANI@cpan.org>
2018-02-22 18:55:01 +0100

    [ergodox mfontani] move CAG key to \ on RHS

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   =  |Backspac|
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  /   |           |   \  |   Y  |   U  |   I  |   O  |   P  |   '"   |
- * |--------+------+------+------+------+------| SYMB |           |      |------+------+------+------+------+--------|
- * | CAG `~ |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/Cmd |
+ * |--------+------+------+------+------+------| SYMB |           |  CAG |------+------+------+------+------+--------|
+ * |  `~    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/Cmd |
  * |--------+------+------+------+------+------|  ~/  |           | LEAD |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  UP  | / Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -73,20 +73,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,            KC_2,    KC_3,    KC_4,    KC_5, KC_6,
-        KC_TAB,         KC_Q,            KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
-        LCAG_T(KC_GRV), KC_A,            KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,        KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, M_HOME,
-        KC_LCTRL,       LT(SYMB,KC_GRV), KC_SLSH, KC_LALT, KC_LGUI,
+        KC_ESC,   KC_1,            KC_2,    KC_3,    KC_4,    KC_5, KC_6,
+        KC_TAB,   KC_Q,            KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
+        KC_GRV,   KC_A,            KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT,  KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, M_HOME,
+        KC_LCTRL, LT(SYMB,KC_GRV), KC_SLSH, KC_LALT, KC_LGUI,
                                                       KC_RALT,KC_QUOT,
                                                               KC_HOME,
                                                 KC_SPC,KC_LGUI,KC_END,
         // right hand
-        KC_7,    KC_8, KC_9,            KC_0,            KC_MINS, KC_EQL,           KC_BSPC,
-        KC_BSLS, KC_Y, KC_U,            KC_I,            KC_O,    KC_P,             KC_QUOT,
-                 KC_H, KC_J,            KC_K,            KC_L,    LT(MDIA,KC_SCLN), GUI_T(KC_ENT),
-        KC_LEAD, KC_N, KC_M,            KC_COMM,         KC_DOT,  KC_UP,            SFT_T(KC_SLSH),
-                       ALGR_T(KC_LBRC), ALGR_T(KC_RBRC), KC_LEFT, KC_DOWN,          KC_RIGHT,
+        KC_7,            KC_8, KC_9,            KC_0,            KC_MINS, KC_EQL,           KC_BSPC,
+        LCAG_T(KC_BSLS), KC_Y, KC_U,            KC_I,            KC_O,    KC_P,             KC_QUOT,
+                         KC_H, KC_J,            KC_K,            KC_L,    LT(MDIA,KC_SCLN), GUI_T(KC_ENT),
+        KC_LEAD,         KC_N, KC_M,            KC_COMM,         KC_DOT,  KC_UP,            SFT_T(KC_SLSH),
+                               ALGR_T(KC_LBRC), ALGR_T(KC_RBRC), KC_LEFT, KC_DOWN,          KC_RIGHT,
         KC_DELT, KC_RALT,
         KC_PGUP,
         KC_PGDN, KC_ENT, KC_SPC
