5df0044a4c62997e1e2ff0b532846072d17c20cf
Marco Fontani <MFONTANI@cpan.org>
2018-06-28 23:54:29 +0200

    [build-mfontani.sh] add teensy_loader_cli step
    ... as I am now on Linux, and can do that easily.

--- build-mfontani.sh
+++ build-mfontani.sh
@@ -4,4 +4,6 @@ set -e
 
 docker build . -t qmk
 docker run -e keyboard=ergodox_ez -e subproject=mfontani -e keymap=mfontani --rm -v "$(pwd):/qmk:rw" qmk
-mv ergodox_ez_mfontani.hex "$( date +%s )_ergodox_ez_mfontani.hex"
+new_hex="$( date +%s )_ergodox_ez_mfontani.hex"
+mv ergodox_ez_mfontani.hex "$new_hex"
+teensy_loader_cli --mcu=TEENSY2 -v -w "$new_hex"
