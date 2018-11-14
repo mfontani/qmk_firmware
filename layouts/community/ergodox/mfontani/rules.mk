cfecddb8c3f98422f4032c929df1829fec1b8bab
Marco Fontani <MFONTANI@cpan.org>
2018-11-14 12:37:36 +0100

    [ergodox mfontani] disable tap dances

--- layouts/community/ergodox/mfontani/rules.mk
+++ layouts/community/ergodox/mfontani/rules.mk
@@ -1,5 +1,5 @@
 # Only lists overrides; see keyboards/ergodox_ez/rules.mk for defaults
 CONSOLE_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+# TAP_DANCE_ENABLE = yes
 OPT_DEFS += -DUSER_PRINT
 LEADER_ENABLE = yes
