af1ae126e9871a6ff783ea745cdb084d23824da2
Marco Fontani <MFONTANI@cpan.org>
2019-01-02 12:50:49 +0100

    [build-mfontani.sh] update for latest upstream Dockerfile

--- build-mfontani.sh
+++ build-mfontani.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build . -t qmk
-docker run -e keyboard=ergodox_ez -e subproject=mfontani -e keymap=mfontani --rm -v "$(pwd):/qmk:rw" qmk
+docker run --rm -v "$(pwd):/qmk_firmware:rw" qmk make ergodox_ez:mfontani
 new_hex="$( date +%s )_ergodox_ez_mfontani.hex"
 mv ergodox_ez_mfontani.hex "$new_hex"
 teensy_loader_cli --mcu=TEENSY2 -v -w "$new_hex"
