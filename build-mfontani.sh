7398a513f0f7df45d327ffcf23c8f7795d794f7d
Marco Fontani <MFONTANI@cpan.org>
2019-02-04 14:50:13 +0100

    [build-mfontani.sh] skip building every time
    ... as the current directory is always passed through

--- build-mfontani.sh
+++ build-mfontani.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build . -t qmk
+# docker build . -t qmk
 docker run --rm -v "$(pwd):/qmk_firmware:rw" qmk make ergodox_ez:mfontani
 new_hex="$( date +%s )_ergodox_ez_mfontani.hex"
 mv ergodox_ez_mfontani.hex "$new_hex"
