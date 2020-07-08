0a7e330ad465403fdc67fa3cd60bf6bf2882bfbf
Marco Fontani <MFONTANI@cpan.org>
2020-07-08 09:24:21 +0200

    [ergodox mfontani] use left LEAD as ~ key

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  /   |           |   \  |   Y  |   U  |   I  |   O  |   P  |   '"   |
  * |--------+------+------+------+------+------| SYMB |           |  CAG |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/Ctrl|
- * |--------+------+------+------+------+------| LEAD |           | LEAD |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  UP  | / Shift|
+ * |--------+------+------+------+------+------|   `  |           | LEAD |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |   ~  |           |      |   N  |   M  |   ,  |   .  |  UP  | / Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |C_S_T |SYMB/`| AltG | Alt  |  Cmd |                                       |   [  |   ]  | LEFT | DOWN | RIGHT|
  *   `----------------------------------'                                       `----------------------------------'
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,        KC_1,            KC_2,    KC_3,    KC_4,    KC_5, KC_6,
         KC_TAB,        KC_Q,            KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
         KC_LCTRL,      KC_A,            KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,       KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, KC_LEAD,
+        KC_LSFT,       KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,
         C_S_T(KC_NO),  LT(SYMB,KC_GRV), KC_RALT, KC_LALT, KC_LGUI,
                                                       KC_SLSH,KC_QUOT,
                                                               KC_HOME,
