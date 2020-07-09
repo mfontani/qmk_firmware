fe3e8c306cd7b2b1a2b92b4c332c2e36f631f683
Marco Fontani <MFONTANI@cpan.org>
2020-07-09 15:16:26 +0200

    [ergodox mfontani] resume ent/cmd and separate ctrl

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -65,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  /   |           |   \  |   Y  |   U  |   I  |   O  |   P  |   '"   |
  * |--------+------+------+------+------+------| SYMB |           |  CAG |------+------+------+------+------+--------|
- * |Ctrl/`~ |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/Ctrl|
+ * |Ctrl/`~ |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / MD|Ent/CMD |
  * |--------+------+------+------+------+------|   ~  |           | LEAD |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  UP  | / Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |C_S_T |SYMB/`| AltG | Alt  |  Cmd |                                       |   [  |   ]  | LEFT | DOWN | RIGHT|
+ *   | Ctrl |SYMB/`| AltG | Alt  |  Cmd |                                       |   [  |   ]  | LEFT | DOWN | RIGHT|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  /?  |  '"  |       | Del  | AltGr|
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,            KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
         CTL_T(KC_GRV), KC_A,            KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,       KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, KC_TILD,
-        C_S_T(KC_NO),  LT(SYMB,KC_GRV), KC_RALT, KC_LALT, KC_LGUI,
+        KC_LCTL,       LT(SYMB,KC_GRV), KC_RALT, KC_LALT, KC_LGUI,
                                                       KC_SLSH,KC_QUOT,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_7,            KC_8, KC_9,    KC_0,    KC_MINS,          KC_EQL,           KC_BSPC,
 #endif
         LCAG_T(KC_BSLS), KC_Y, KC_U,    KC_I,    KC_O,             KC_P,             KC_QUOT,
-                         KC_H, KC_J,    KC_K,    KC_L,             LT(MDIA,KC_SCLN), CTL_T(KC_ENT),
+                         KC_H, KC_J,    KC_K,    KC_L,             LT(MDIA,KC_SCLN), GUI_T(KC_ENT),
         KC_LEAD,         KC_N, KC_M,    KC_COMM, KC_DOT,           KC_UP,            SFT_T(KC_SLSH),
                                KC_LBRC, KC_RBRC, KC_LEFT,          KC_DOWN,          KC_RIGHT,
         KC_DELT, KC_RALT,
