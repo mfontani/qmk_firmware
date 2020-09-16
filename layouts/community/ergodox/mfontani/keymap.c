db30a3c6a70b29bc00b8b698e71c173db4775c47
Marco Fontani <MFONTANI@cpan.org>
2020-09-16 11:58:54 +0200

    [ergodox mfontani] enable LTO optimisations

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -232,10 +232,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
-
 #ifdef MFONTANI_OSX_RALT_UNICODE
 // I've mapped Alt+Cmd+Space to switch to next layout.
 // I usually stay in US layout, so switching gets me into Unicode layout, and
--- layouts/community/ergodox/mfontani/rules.mk
+++ layouts/community/ergodox/mfontani/rules.mk
@@ -1,5 +1,7 @@
 # Only lists overrides; see keyboards/ergodox_ez/rules.mk for defaults
+LTO_ENABLE = yes
 CONSOLE_ENABLE = yes
 # TAP_DANCE_ENABLE = yes
 OPT_DEFS += -DUSER_PRINT
 LEADER_ENABLE = yes
+EXTRAFLAGS += -flto
