16a1c78150d8ad2d27e1861c0da89182f9fb6f76
Marco Fontani <MFONTANI@cpan.org>
2020-11-19 17:15:51 +0100

    [ergodox mfontani] resume ctrl on bottom left

--- layouts/community/ergodox/mfontani/keymap.c
+++ layouts/community/ergodox/mfontani/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,            KC_W,    KC_E,    KC_R,    KC_T, LT(SYMB,KC_SLSH),
         KC_LCTL,       KC_A,            KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,       KC_Z,            KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,
-        _______,       LT(SYMB,KC_GRV), KC_RALT, KC_LALT, KC_LGUI,
+        KC_LCTL,       LT(SYMB,KC_GRV), KC_RALT, KC_LALT, KC_LGUI,
                                                       KC_SLSH,KC_QUOT,
                                                               KC_HOME,
                                                KC_SPC,KC_LCTL,KC_END,
